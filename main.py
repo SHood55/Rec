84d991e36afb098d48d363361d5d23aa83fbf722
Wilhelm W Schive <wilumium@hotmail.com>
2015-03-12 16:22:54 +0100

    added playstoreapi to get similar apps

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import socket
 import sys
 from thread import *
 import traceback
+import json
+import requests
 import select
 import BaseHTTPServer, SimpleHTTPServer, ssl
 
@@ -13,16 +15,34 @@ import BaseHTTPServer, SimpleHTTPServer, ssl
 def main() :
 
 #    name = "com.hdezninirola.frequency"
-#     name = "no.nrk.yr"
+    name = "no.nrk.yr"
 #     name = "com.rovio.angrybirds"
 #     dir = "/Users/Wschive/Desktop/"
 #     name = "com.kabam.underworldandroid"
-    getApps.run("com.bitdefender.clueful")
-    risk.run("com.bitdefender.clueful")
+#     getApps.run("com.bitdefender.clueful")
+#     risk.run("com.bitdefender.clueful")
 #     recommender.recommend()
 
+#     s = requests.Session()
+# #     s.auth=('wilhelmws@gmail.com', '1.9.Alpha')
+#     s.post("https://accounts.google.com/ServiceLogin", {"Email":'wilhelmws@gmail.com', "Passwd":'1.9.Alpha'})
+#     print s
+#     value = {"id":"com.squareenix.smoothieswipe"}
+# #     r = s.get("https://play.google.com/store/apps/details", params = value)
+# #     print r
+#
+#     g = s.get("http://play.google.com/store/apps/similar", params = value)
+#     print g
+#     print(g.url)
+#     print g.content
 #     server.run()
 
+#     works, but limited amount of calls
+#     apiKey = {"key" :"9494f057c1a1a67ab30e5e7afdc6afe2"}
+#     r = requests.get("http://api.playstoreapi.com/v1.1/apps/"+name, params = apiKey)
+#     data = json.loads(r.content)
+#     print data
+#     print data["recommendedApps"]
     print "it ran!"
 
 
